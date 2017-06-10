2ee4c53741d3f20c60f620468a527cedbd0b7eb4
Nicola Squartini <tensor5@gmail.com>
2017-06-11 05:51:15 +0900

    Endorsement request notifications

--- vipPrice/server/publications.js
+++ vipPrice/server/publications.js
@@ -1,4 +1,8 @@
+import Notifications from '/imports/Notifications'
+
 Meteor.publish('allUsers', () => {
   const allUsers = Meteor.users.find()
   return allUsers
 })
+
+Meteor.publish('notifications', () => Notifications.find())
