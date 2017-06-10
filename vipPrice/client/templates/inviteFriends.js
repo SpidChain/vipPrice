3fdb0a1a8f9ff0504c10ad9d0bfc92be850c4d7e
Nicola Squartini <tensor5@gmail.com>
2017-06-11 04:45:35 +0900

    Radio buttons

--- vipPrice/client/templates/inviteFriends.js
+++ vipPrice/client/templates/inviteFriends.js
@@ -1,3 +1,13 @@
 Template.inviteFriends.helpers({
    users: () => Meteor.users.find()
 })
+
+Template.inviteFriends.events({
+    'submit': (e) => {
+        e.preventDefault()
+        const checked = document.querySelector('input[name="users"]:checked')
+        if (checked) {
+            console.log(checked.value);
+        }
+    }
+})
