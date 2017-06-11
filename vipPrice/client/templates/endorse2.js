55f57ee3e805e58d2a56e95797b5da0292b3f656
Federico Squartini <federico.squartini@gmail.com>
2017-06-11 12:02:10 +0200

    Fix subscriptions

--- vipPrice/client/templates/endorse2.js
+++ vipPrice/client/templates/endorse2.js
@@ -1,6 +1,6 @@
 const createEndorsements = (user) => [
-  `${user} piacciono le automobili vintage`,
-  `${user} andra in vacanza al mare quest'estate`,
+  `A ${user} piacciono le automobili vintage`,
+  `A ${user} andra in vacanza al mare quest'estate`,
   `${user} si sta per sposare`,
 ]
 
@@ -10,14 +10,17 @@ let n = 3
 Template.endorse2.onCreated(function () {
   this.autorun(() => {
     if( this.subscriptionsReady()) {
+      if(Meteor.user()) {
       console.log("subscriptionsready")
       console.log("Meteor.user()", Meteor.user())
       const endorser = Meteor.user().profile.endorser
       if(endorser) {
       console.log("endorser is", endorser)
-      const endorserName = Meteor.users.find(endorser).profile.name
+      const endorserName = Meteor.users.findOne(endorser).profile.name
       const endorsements = createEndorsements(endorserName)
+      console.log(endorsements)
       this.endorsements = endorsements
+      }
       }
     }
   }
@@ -25,9 +28,12 @@ Template.endorse2.onCreated(function () {
 })
 
 Template.endorse2.helpers({
-  endorsement: () =>{
+  endorsement: function (){
+    console.log(Template.instance())
     const e = Template.instance().endorsements[i]
     //n = (n + 1) % 3
     return e
   }
 })
+
+
