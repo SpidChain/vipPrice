01492ade83a5310a4d363dbde176f8c742d25fc4
Federico Squartini <federico.squartini@gmail.com>
2017-06-11 17:31:56 +0200

    Add new claims

--- vipPrice/imports/claims.js
+++ vipPrice/imports/claims.js
@@ -1,6 +1,12 @@
 export default [
-    'sa giocare a tennis',
-    'va in barca a vela',
-    'sa andare a cavallo'
+    'Sa giocare a tennis',
+    'Va in barca a vela',
+    "Si sposera' tra 3 mesi",
+    "Andra' in vacanza alle Hawaii",
+    "E' abbonato al teatro dell'opera",
+    'Ama viaggiare',
+    'Sa andare a cavallo',
+    "Vuole acquistare una nuova auto d'epoca",
+    'Gli piace fare trading'
 ]
 
