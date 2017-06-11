59444122665856962b86ee9d79d45f4144767fe6
Nicola Squartini <tensor5@gmail.com>
2017-06-11 18:29:51 +0900

    Change UI in myEndorsements

--- vipPrice/client/templates/myEndorsements.js
+++ vipPrice/client/templates/myEndorsements.js
@@ -1,5 +1,5 @@
 Template.myEndorsements.helpers({
-    endorsements: () => Meteor.user().profile.endorsements
+    endorsements: () => Meteor.user().profile.endorsements.filter((e) => e.share === undefined)
 })
 
 Template.myEndorsements.events({
@@ -14,6 +14,21 @@ Template.myEndorsements.events({
         const newEndorsements =
             endorsements.map((e) => sharedEndorsements.indexOf(e.claim) !== -1
                 ? {...e, share: true}
+                : e
+            )
+        Meteor.users.update(Meteor.userId(), {$set: {'profile.endorsements': newEndorsements}})
+    },
+    
+    'click i': (e) => {
+        const claim = e.target.getAttribute('data-claim')
+        const endorsements = Meteor.user().profile.endorsements
+        const action = e.target.getAttribute('data-action')
+        const share = action === 'share'
+            ? true
+            : false
+        const newEndorsements =
+            endorsements.map((e) => e.claim === claim
+                ? {...e, share: share}
                 : e
             )
         Meteor.users.update(Meteor.userId(), {$set: {'profile.endorsements': newEndorsements}})
